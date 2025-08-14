1353b3d9bd904fd01bde73e4ac6ba71c4b8bee28
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-08-14 15:41:32 -0400

    datos de contacto links

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+
+
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const MyApp());
 
@@ -28,6 +32,44 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) async {
+            String contact = "5351251511";
+              String text = 'Hola, me gusto tu juego hiragana';
+              String androidUrl = "whatsapp://send?phone=$contact&text=$text";
+
+            // if (mounted) { // 'mounted' comprueba que el widget está en pantalla
+            //   ScaffoldMessenger.of(context).showSnackBar(
+            //     SnackBar(
+            //       content: Text('Interceptado: ${request.url} ${request.url.startsWith("https://wa.me/")} ${await canLaunchUrl(Uri.parse(androidUrl))}'),
+            //       duration: const Duration(seconds: 3),
+            //     ),
+            //   );
+            // }
+              
+              if (request.url.startsWith("https://wa.me/")){
+                if (await canLaunchUrl(Uri.parse(androidUrl))) {
+                  await launchUrl(Uri.parse(androidUrl));
+                  return NavigationDecision.prevent;
+                }
+              }
+
+              if (request.url.startsWith("mailto:") ||
+                  request.url.startsWith("tel:") ||
+                  request.url.startsWith("https://wa.me/") ||
+                  request.url.contains("linkedin.com") ||
+                  request.url.contains("github.com")) {
+                
+                if (await canLaunchUrl(Uri.parse(request.url))) {
+                  await launchUrl(Uri.parse(request.url), mode: LaunchMode.externalApplication);
+                }
+                return NavigationDecision.prevent;
+              }
+              return NavigationDecision.navigate;
+            },
+        ),
+      )
       ..loadFlutterAsset('assets/web/index.html');  // punto de entrada
   }
 
